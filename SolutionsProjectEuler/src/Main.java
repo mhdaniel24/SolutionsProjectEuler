adbd14969226e6d934ff08ed2b4af6e713c1bdd2
Andy Daniel Martinez <mhdaniel24@gmail.com>
2015-07-28 02:14:42 -0400

    Solutions to Problem1 and Problem2

--- SolutionsProjectEuler/src/Main.java
+++ SolutionsProjectEuler/src/Main.java
@@ -1,7 +1,11 @@
+import Solutions.*;
 
 public class Main {
 
 	public static void main(String[] args) {
+		new Problem1().solve();
+		new Problem2().solve();
+		
 
 	}
 
---
+++ SolutionsProjectEuler/src/Solutions/Problem1.java
@@ -0,0 +1,33 @@
+/*
+ * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+ * The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+package Solutions;
+
+public class Problem1 {
+	private double solution;
+	
+	public void solve(){ 
+		int lastnum = 999;
+       	int number = 0;//increasing numbers
+       	int accumulator = 0;
+       
+       	while (number < lastnum)
+       	{
+       		number = number +1;
+           
+        	if((number%3 == 0)||(number%5 == 0))//choosing them if they are divisible by 3 or 5
+        	{
+        		accumulator = accumulator + number;
+        	}
+       }
+       	solution = accumulator *1.0;
+       	
+       	printSolution();
+	}
+	
+	private void printSolution(){
+		System.out.println(Problem1.class.getSimpleName() + " solution = " + solution);
+	}
+	
+}
